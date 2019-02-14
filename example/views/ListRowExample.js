9aecbda720ff239eaf0de1120a98ab8155aba324
zhanjian <zhanjian@apexsoft.com>
2019-02-14 16:00:30 +0800

    修改文件格式2019.2.14 16:00

--- example/views/ListRowExample.js
+++ example/views/ListRowExample.js
@@ -1,11 +1,10 @@
 // ListRowExample.js
 
-'use strict';
 
-import React, {Component} from 'react';
-import {View, ScrollView, Image, Text} from 'react-native';
+import React, { Component } from 'react';
+import { View, ScrollView, Image, Text } from 'react-native';
 
-import {NavigationPage, ListRow, Label} from 'teaset';
+import { NavigationPage, ListRow, Label } from 'teaset';
 
 export default class ListRowExample extends NavigationPage {
 
@@ -21,19 +20,29 @@ export default class ListRowExample extends NavigationPage {
 
   renderPage() {
     return (
-      <ScrollView style={{flex: 1}}>
-        <View style={{height: 20}} />
+      <ScrollView style={{ flex: 1 }}>
+        <View style={{ height: 20 }} />
         <ListRow title='Title' detail='Detail' topSeparator='full' />
-        <ListRow title={<Label style={{fontSize: 18, color: '#31708f'}} text='Custom title' />} />
-        <ListRow title='Custom detail' detail={
-          <View style={{backgroundColor: '#5bc0de', width: 60, height: 24, borderRadius: 4}} />
-        } />
-        <ListRow title='Long detail' detail={
-          'React Native enables you to build world-class application experiences on native platforms using a consistent developer experience based on JavaScript and React.'
-        } />
-        <ListRow title='Title place top' detail={
-          'React Native enables you to build world-class application experiences on native platforms using a consistent developer experience based on JavaScript and React.'
-        } titlePlace='top' />
+        <ListRow title={<Label style={{ fontSize: 18, color: '#31708f' }} text='Custom title' />} />
+        <ListRow
+          title='Custom detail'
+          detail={
+            <View style={{ backgroundColor: '#5bc0de', width: 60, height: 24, borderRadius: 4 }} />
+          }
+        />
+        <ListRow
+          title='Long detail'
+          detail={
+            'React Native enables you to build world-class application experiences on native platforms using a consistent developer experience based on JavaScript and React.'
+          }
+        />
+        <ListRow
+          title='Title place top'
+          detail={
+            'React Native enables you to build world-class application experiences on native platforms using a consistent developer experience based on JavaScript and React.'
+          }
+          titlePlace='top'
+        />
         <ListRow title='Icon' icon={require('../icons/config.png')} />
         <ListRow title='Accessory indicator' accessory='indicator' />
         <ListRow title='Custom accessory' accessory={<Image source={require('../icons/location.png')} />} />
@@ -43,12 +52,12 @@ export default class ListRowExample extends NavigationPage {
           // detail='Swipe to show action buttons'
           swipeActions={[
             <ListRow.SwipeActionButton title='Cancel' />,
-            <ListRow.SwipeActionButton title='Remove' type='danger' onPress={() => alert('Remove')}/>,          
+            <ListRow.SwipeActionButton title='Remove' type='danger' onPress={() => alert('Remove')} />,
           ]}
           bottomSeparator='full'
-          >
-            <Text>sadasdsa</Text>
-          </ListRow>
+        >
+          <Text>sadasdsa</Text>
+        </ListRow>
       </ScrollView>
     );
   }
